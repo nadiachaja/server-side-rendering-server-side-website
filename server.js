a4bd41b1d865fb6257c7909afa93dba79e77d94c
nadiachaja <nadiagaja13@gmail.com>
2025-03-10 15:28:15 +0100

    #3 styling desktop homepagina #2 404 aangemaakt, register in de loop van de cadeaus en de loop gescheiden maken voor de  verschillende pagina's
    De stylesheet font op goede plek.
    Styling voor desktop.
    
    Klein begin detailpagina

--- server.js
+++ server.js
@@ -67,3 +67,8 @@ app.listen(app.get('port'), function () {
   // Toon een bericht in de console en geef het poortnummer door
   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
+
+
+app.use((req, res, next) => {
+  res.status(404).send("Sorry kan de pagina niet vinden")
+})
